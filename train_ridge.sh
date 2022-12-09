169a74ef1dfb06fa58fb38ac7e90168eb28cc2a8
Andre Warken <andre.warken@sentryc.com>
2022-12-09 15:08:37 +0100

    add data workers

--- train_ridge.sh
+++ train_ridge.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-python train.py  --batch-size 16 --epochs 300 --data data/ridge_100.yaml \
+python train.py  --workers 8 --batch-size 16 --epochs 300 --data data/ridge_100.yaml \
     --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/ridge.yaml \
     --weights yolov7x_training.pt --name yolov7x-ridge-100
 
-python train.py  --batch-size 16 --epochs 300 --data data/ridge_300.yaml \
+python train.py  --workers 8  --batch-size 16 --epochs 300 --data data/ridge_300.yaml \
     --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/ridge.yaml \
     --weights yolov7x_training.pt --name yolov7x-ridge-300
 
-python train.py  --batch-size 16 --epochs 300 --data data/ridge.yaml \
+python train.py --workers 8  --batch-size 16 --epochs 300 --data data/ridge.yaml \
     --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/ridge.yaml \
     --weights yolov7x_training.pt --name yolov7x-ridge-500
